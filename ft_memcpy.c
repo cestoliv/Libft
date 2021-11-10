65233f0563258485b95865bb0b29f9c84a1ae47b
Olivier Cartier <me@cestoliv.com>
2021-11-10 14:25:01 +0100

    securise memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:16:52 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/10 09:04:42 by ocartier         ###   ########lyon.fr   */
+/*   Updated: 2021/11/10 14:24:45 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char	*dst_data;
 	char	*src_data;
 
+	if (!src && !dst)
+		return (NULL);
 	cur = 0;
 	dst_data = (char *)dst;
 	src_data = (char *)src;
