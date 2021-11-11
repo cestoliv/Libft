0bfd5ba371a9d5b492d30e0069d2c9e4ce04e12a
Olivier Cartier <me@cestoliv.com>
2021-11-11 09:39:30 +0100

    Securise strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 11:45:20 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/10 09:08:11 by ocartier         ###   ########lyon.fr   */
+/*   Updated: 2021/11/11 09:39:15 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 	char	*join;
 
+	if (!s1 || !s2)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	join = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
