f8307ba549a798c169e257938ae9f2b85fd834f5
Olivier Cartier <me@cestoliv.com>
2021-11-11 09:34:42 +0100

    Retrying to securise memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 14:12:17 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/10 14:28:27 by ocartier         ###   ########.fr       */
+/*   Updated: 2021/11/11 09:33:58 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char	*data_dst;
 	char	*data_src;
 
+	if (!src && !dst)
+		return (NULL);
 	if (src > dst)
 		dst = ft_memcpy(dst, src, len);
 	else
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 11:22:13 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/10 09:09:57 by ocartier         ###   ########lyon.fr   */
+/*   Updated: 2021/11/10 14:48:14 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,22 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
 
-	if (ft_strlen(s) < start)
-		return ("");
-	sub = (char *)malloc((len + 1) * sizeof(char));
-	if (!sub)
+	if (!s)
 		return (NULL);
-	sub[len] = 0;
-	while (len-- > 0)
-		sub[len] = s[start + len];
+	if (ft_strlen(s) < start)
+	{
+		sub = ft_calloc(1, sizeof(char));
+		if (!sub)
+			return (NULL);
+	}
+	else
+	{
+		sub = (char *)malloc((len + 1) * sizeof(char));
+		if (!sub)
+			return (NULL);
+		sub[len] = 0;
+		while (len-- > 0)
+			sub[len] = s[start + len];
+	}
 	return (sub);
 }
