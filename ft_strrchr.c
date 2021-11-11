2f3fdd076d1233cc74939d12a7c21a95ca36decb
Olivier Cartier <me@cestoliv.com>
2021-11-11 10:22:19 +0100

    Cast problem in strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:15:55 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/03 12:20:41 by ocartier         ###   ########lyon.fr   */
+/*   Updated: 2021/11/11 10:22:03 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strrchr(const char *s, int c)
 	ret = 0;
 	while (s[cur])
 	{
-		if (s[cur] == c)
+		if (s[cur] == (unsigned char)c)
 			ret = (char *)(s + cur);
 		cur++;
 	}
-	if (s[cur] == c)
+	if (s[cur] == (unsigned char)c)
 		ret = (char *)(s + cur);
 	return (ret);
 }
