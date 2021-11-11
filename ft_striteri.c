c0e9504c70d9bbbc0e6027e2534590da54f307f4
Olivier Cartier <me@cestoliv.com>
2021-11-11 09:56:40 +0100

    il faut etre debile

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 11:38:55 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/11 09:55:41 by ocartier         ###   ########.fr       */
+/*   Updated: 2021/11/11 09:56:31 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	int	cur;
 
-	if (s && f)
+	if (!s || !f)
 		return ;
 	cur = 0;
 	while (s[cur])
