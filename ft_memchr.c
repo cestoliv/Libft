13f5197454a1c47db9a4b5e768568d10bb93f632
Olivier Cartier <me@cestoliv.com>
2021-11-11 10:37:39 +0100

    Norme error

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ocartier <ocartier@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:54:33 by ocartier          #+#    #+#             */
-/*   Updated: 2021/11/11 10:15:45 by ocartier         ###   ########.fr       */
+/*   Updated: 2021/11/11 10:37:24 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	cur;
+	size_t				cur;
 	unsigned char const	*data;
 
 	cur = 0;
